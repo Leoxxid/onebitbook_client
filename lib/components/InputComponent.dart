8c01a915d131ef8343f1357f89dc331d37b54da7
Kaique Munhoz <munhoz13km@gmail.com>
2019-06-30 22:34:23 -0300

    Passando label e hint para o component de input

--- lib/components/InputComponent.dart
+++ lib/components/InputComponent.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 
 class InputComponent extends StatelessWidget {
   final messageEmptyInput = 'Você precisa preencher este campo...';
+  final hint;
+  final label;
+
+  InputComponent(this.hint, this.label) {}
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      validator: (value) {
-        if (value.isEmpty) {
-          return messageEmptyInput;
-        }
-        return null;
-      },
-    );
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: TextFormField(
+          decoration: InputDecoration(
+            hintText: hint,
+            labelText: label,
+          ),
+          validator: (value) {
+            if (value.isEmpty) {
+              return messageEmptyInput;
+            }
+            return null;
+          },
+        ));
   }
 }
--- lib/screens/RegisterBookScreen.dart
+++ lib/screens/RegisterBookScreen.dart
@@ -10,26 +10,27 @@ class RegisterBookScreen extends StatefulWidget {
 
 class RegisterBookScreenState extends State<RegisterBookScreen> {
   final _formKey = GlobalKey<FormState>();
+  final registerBook = "Cadastre um Livro";
+
+  final typeTheNameOfAuthor = "Digite o nome do Autor";
+  final nameOfTheAuthor = "Nome do Autor";
+
+  // final  = "Digite o nome do autor";
+  // final placeholderBook = "Digite o nome do livro";
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add a new Dog'),
-        backgroundColor: Colors.black87,
+        title: Text(registerBook),
       ),
       body: Container(
-        color: Colors.black54,
         child: Padding(
           padding: const EdgeInsets.symmetric(
             vertical: 8.0,
             horizontal: 32.0,
           ),
           child: Column(
-            children: [
-              Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: InputComponent()),
-            ],
+            children: [InputComponent(typeTheNameOfAuthor, nameOfTheAuthor)],
           ),
         ),
       ),
