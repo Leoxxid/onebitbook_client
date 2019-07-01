512ff69d71ad5bd1f9638a76d7e7370efce0066a
Kaique Munhoz <munhoz13km@gmail.com>
2019-06-30 23:00:01 -0300

    Feat: Ajustando formato do card da lista

--- lib/components/BookListComponent.dart
+++ lib/components/BookListComponent.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
 class BookListComponent extends StatelessWidget {
+  final edit = 'Editar';
+  final delete = 'Deletar';
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, position) {
         return Card(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              position.toString(),
-              style: TextStyle(fontSize: 22.0),
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                leading: Icon(Icons.book),
+                title: Text('Titulo do livro'),
+              ),
+              ButtonTheme.bar(
+                // make buttons use the appropriate styles for cards
+                child: ButtonBar(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text(edit),
+                      onPressed: () {/* ... */},
+                    ),
+                    FlatButton(
+                      child: Text(delete),
+                      onPressed: () {/* ... */},
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         );
       },
