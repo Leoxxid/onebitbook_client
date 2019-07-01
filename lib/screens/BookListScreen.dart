ecba45a629b900c05646e4628ca9dd1d5ad37eaa
Kaique Munhoz <munhoz13km@gmail.com>
2019-07-01 00:54:15 -0300

    Feat: Transformado BookListScreen em um statefulwidget

--- lib/screens/BookListScreen.dart
+++ lib/screens/BookListScreen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:onebitbook_client/components/BookListComponent.dart';
 import 'package:onebitbook_client/components/ButtonGoToRegisterBookComponent.dart';
+import 'package:onebitbook_client/services/Repository.dart';
 
-class BookListScreen extends StatelessWidget {
+class BookListScreen extends StatefulWidget {
+  final Repository _repository;
+
+  @override
+  _BuildListState createState() => _BuildListState();
+}
+
+class _BuildListState extends State<BookListScreen> {
   final titleScreen = "Lista de Livros";
 
   @override
